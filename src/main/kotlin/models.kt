1433eff57314e37d94a33337198dc3c07e3d7a5e
Julien Terrier <julien.terrier@orange.com>
2020-03-30 16:16:02 +0200

    Implémente la règle de calcul du score pour une quille tombée et refactore l'existant

--- src/main/kotlin/models.kt
+++ src/main/kotlin/models.kt
@@ -1,7 +1,53 @@
-class Lancer(quillesTombees: Set<Quille>) {
-    val score: Int = quillesTombees.size
+import Quille.Q1
+import Quille.Q10
+import Quille.Q11
+import Quille.Q12
+import Quille.Q2
+import Quille.Q3
+import Quille.Q4
+import Quille.Q5
+import Quille.Q6
+import Quille.Q7
+import Quille.Q8
+import Quille.Q9
+
+class Lancer(
+    q1: Boolean = false,
+    q2: Boolean = false,
+    q3: Boolean = false,
+    q4: Boolean = false,
+    q5: Boolean = false,
+    q6: Boolean = false,
+    q7: Boolean = false,
+    q8: Boolean = false,
+    q9: Boolean = false,
+    q10: Boolean = false,
+    q11: Boolean = false,
+    q12: Boolean = false
+) {
+    private val quillesTombees = mutableSetOf<Quille>().apply {
+        if (q1) add(Q1)
+        if (q2) add(Q2)
+        if (q3) add(Q3)
+        if (q4) add(Q4)
+        if (q5) add(Q5)
+        if (q6) add(Q6)
+        if (q7) add(Q7)
+        if (q8) add(Q8)
+        if (q9) add(Q9)
+        if (q10) add(Q10)
+        if (q11) add(Q11)
+        if (q12) add(Q12)
+    }.toSet()
+
+    val score = if (quillesTombees.size == 1) {
+        quillesTombees.first().valeur
+    } else {
+        quillesTombees.size
+    }
+
 }
 
-enum class Quille {
-    Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12
+enum class Quille(val valeur: Int) {
+    Q1(1), Q2(2), Q3(3), Q4(4), Q5(5), Q6(6), Q7(7), Q8(8), Q9(9), Q10(10), Q11(11), Q12(12)
 }
--- src/test/kotlin/LancerTest.kt
+++ src/test/kotlin/LancerTest.kt
@@ -1,8 +1,3 @@
-import Quille.Q1
-import Quille.Q10
-import Quille.Q2
-import Quille.Q7
-import Quille.Q9
 import io.kotlintest.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -10,9 +5,21 @@ class LancerTest {
 
     @Test
     fun `score pour plusieurs quilles tombees`() {
-        Lancer(quillesTombees = setOf(Q1, Q9)).score shouldBe 2
-        Lancer(quillesTombees = setOf(Q1, Q9, Q2, Q7, Q10)).score shouldBe 5
-        Lancer(quillesTombees = setOf(Q1, Q9, Q2)).score shouldBe 3
-        Lancer(quillesTombees = setOf(Q1, Q9, Q9)).score shouldBe 2 // TODO
+        Lancer(q1 = true, q9 = true).score shouldBe 2
+        Lancer(q1 = true, q9 = true, q2 = true, q7 = true, q10 = true).score shouldBe 5
+        Lancer(q1 = true, q9 = true, q2 = true).score shouldBe 3
+        Lancer(q1 = true, q9 = true).score shouldBe 2
+    }
+
+    @Test
+    fun `score pour une seule quilles tombee`() {
+        Lancer(q1 = true).score shouldBe 1
+        Lancer(q5 = true).score shouldBe 5
+        Lancer(q12 = true).score shouldBe 12
+    }
+
+    @Test
+    fun `score nul pour un echec`() {
+        Lancer().score shouldBe 0
     }
 }
